e0a379adb69c4cc434383cd36e2aa6eb1907eb35
Deshan Nawanjana <deshan.uok@gmail.com>
2022-04-29 02:37:52 +0530

    Create Items Jump Methods

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,5 +1,5 @@
 let render_mode = 'free-motion'
-let hold_offset = { then : 0, now : 0, speed : 0, touch : [] }
+let hold_offset = { then : 0, now : 0, speed : 0, touch : [], up : 0, down : 0 }
 let tray_offset = { then : 0, now : 0 }
 let list_offset = { then : 1, now : 0 }
 let list_target = { element : null, direction : null }
@@ -22,6 +22,12 @@ const getEventData = event => {
         speed = ((hold_offset.touch.pop() - hold_offset.touch.pop()) / 2) || 0
     } else if(event.type === 'mouseup' || event.type === 'mouseleave') {
         speed = hold_offset.speed
+    }
+    // store up down
+    if(event.type === 'mousedown' || event.type === 'touchstart') {
+        hold_offset.down = position
+    } else {
+        hold_offset.up = position
     }
     // return event data
     return { position, speed }
@@ -50,14 +56,14 @@ const dragMove = event => {
         // store event data
         const eventData = getEventData(event)
         hold_offset.now = eventData.position
-        hold_offset.speed = eventData.speed * 0.5
+        hold_offset.speed = eventData.speed * 0.3
         // store new tray position
         const hold_gap = hold_offset.now - hold_offset.then
         tray_offset.now = tray_offset.then + hold_gap
     }
 }
 
-const dragEnd = event => {
+const dragEnd = (event, root) => {
     if(render_mode !== 'free-motion') {
         // store speed
         let speed = getEventData(event).speed
@@ -66,7 +72,8 @@ const dragEnd = event => {
         if(speed > max * +1 && speed > 0) { speed = max * +1 }
         if(speed < max * -1 && speed < 0) { speed = max * -1 }
         // speed to value
-        hold_offset.speed = speed
+        if(speed > 0) { setLastItem(root) }
+        if(speed < 0) { setNextItem(root) }
         // change render mode
         render_mode = 'free-motion'
         // save current tray position
@@ -75,18 +82,73 @@ const dragEnd = event => {
     }
 }
 
+const findIndex = (arr, type) => {
+    let value = null
+    let index = 0
+    arr.forEach((item, i) => {
+        if(item !== null & value === null)  {
+            value = item
+            index = i
+        }
+        if(item < value && type === 'min') {
+            value = item
+            index = i
+        } else if(item > value && type === 'max') {
+            value = item
+            index = i
+        }
+    })
+    return index
+}
+
+const setNextItem = root => {
+    let array = getItems(root).map(item => getItemSelectData(root, item))
+    let middx = array.findIndex(x => x.centered)
+    let index = middx === array.length - 1 ? 0 : middx + 1
+    setItemSelect(root, getItems(root)[index])
+}
+
+const setLastItem = root => {
+    let array = getItems(root).map(item => getItemSelectData(root, item))
+    let middx = array.findIndex(x => x.centered)
+    let index = middx === 0 ? array.length - 1 : middx - 1
+    setItemSelect(root, getItems(root)[index])
+}
+
+const setItemSelect = (root, item) => {
+    const data = getItemSelectData(root, item)
+    list_target.element = item
+    list_target.direction = data.direction
+}
+
 const getItemSelectData = (root, item) => {
     const root_crect = root.getBoundingClientRect()
     const item_crect = item.getBoundingClientRect()
     const root_axis = (root_crect.right - root_crect.left) / 2
     const item_axis = item_crect.left + item_crect.width / 2
     const offset = tray_offset.now + (root_axis - item_axis)
+    const centered = item_crect.left < root_axis && item_crect.right > root_axis
     return {
+        element : item,
+        centered : centered,
         root_axis : root_axis,
         item_axis : item_axis,
         direction : item_axis < root_axis ? 'left' : 'right',
         offset : offset
     }
+}
+
+const getCloseItem = root => {
+    let item = null
+    getItems(root).forEach(child => {
+        if(getItemSelectData(root, child).centered) { item = child }
+    })
+    return item
+}
+
+const callbackItem = (root, item) => {
+    const index = getItems(root).indexOf(item)
+    data_inputs.callback(data_inputs.array[index])
 }
 
 const speedForce = root => {
@@ -107,6 +169,7 @@ const speedForce = root => {
         } else {
             hold_offset.speed = 0
             tray_offset.now = data.offset
+            callbackItem(root, list_target.element)
             list_target.element = null
         }
     } else {
@@ -114,6 +177,10 @@ const speedForce = root => {
         tray_offset.now += hold_offset.speed
         if(hold_offset.speed > 0) { hold_offset.speed -= 0.1 }
         if(hold_offset.speed < 0) { hold_offset.speed += 0.1 }
+        if(Math.abs(hold_offset.speed) < 3) {
+            const item = getCloseItem(root)
+            if(item) { setItemSelect(root, item) }
+        }
     }
 }
 
@@ -124,16 +191,15 @@ Scroller.init = (array, generator, callback) => {
     data_inputs = { array, generator, callback }
     // scroller element
     const root = document.querySelector('#Scroller')
-    const tray = root.querySelector('#ScrollerInner')
     // mouse events
     root.addEventListener('mousedown', dragDown)
     root.addEventListener('mousemove', dragMove)
-    root.addEventListener('mouseup', dragEnd)
-    root.addEventListener('mouseleave', dragEnd)
+    root.addEventListener('mouseup', event => dragEnd(event, root))
+    root.addEventListener('mouseleave', event => dragEnd(event, root))
     // touch events
     root.addEventListener('touchstart', dragDown)
     root.addEventListener('touchmove', dragMove)
-    root.addEventListener('touchend', dragEnd)
+    root.addEventListener('touchend', event => dragEnd(event, root))
     // generate items
     Scroller.generate(root)
     // start render
@@ -149,16 +215,16 @@ Scroller.generate = root => {
         const child = data_inputs.generator(item)
         root.appendChild(child)
         child.addEventListener('click', () => {
-            list_target.element = child
-            const item_crect = child.getBoundingClientRect()
-            const epos = item_crect.left + item_crect.width / 2
-            if(epos < axis) { list_target.direction = 'left' }
-            if(epos > axis) { list_target.direction = 'right' }
+            if(hold_offset.down === hold_offset.up) {
+                setItemSelect(root, child)
+            }
         })
     })
     // set initial position
     let item_crect = getItemRect(root, 0)
     tray_offset.now = axis - item_crect.width / 2
+    // call first item callback
+    callbackItem(root, getItems(root)[0])
 }
 
 Scroller.render = root => {
@@ -176,7 +242,6 @@ Scroller.render = root => {
     tray_offset.now = parseInt(tray_offset.now.toFixed(1))
 
     getItems(root).forEach((child, index, array) => {
-        let root_crect = root.getBoundingClientRect()
         let item_crect = getItemRect(root, index)
         let item_width = item_crect.width
         let item_coord = (index * item_width) + tray_offset.now
@@ -200,8 +265,7 @@ Scroller.render = root => {
         child.style.left = item_coord + 'px'
 
         // set current item
-        const axis = (root_crect.right - root_crect.left) / 2
-        if(item_crect.left < axis && item_crect.right > axis) {
+        if(getItemSelectData(root, child).centered) {
             child.setAttribute('focused', '')
         } else {
             child.removeAttribute('focused')
